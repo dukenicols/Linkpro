6c959b9307c921bdc640c9577afe163d53a0b159
Nicolas Duke <nicolas.duque@dodable.com>
2015-08-26 00:12:35 -0300

    Fix #4 Bloqueo otros plugins

--- admin/admin.php
+++ admin/admin.php
@@ -20,20 +20,7 @@ class linkpro_admin {
 		
 	}
 
-	/* Create export download link */
-	function create_export_download_link($echo = false, $setting='linkpro_export_options'){
-		$site_url = get_bloginfo('url');
-		$args = array(
-			$setting => 'safe_download',
-			'nonce' => wp_create_nonce($setting)
-		);
-		$export_url = add_query_arg($args, esc_url($site_url));
-		if ($echo === true)
-			echo '<a href="'.$export_url.'" class="button" target="_blank">'.__('Download Export','linkpro').'</a>';
-		elseif ($echo == 'url')
-			return $export_url;
-		return '<a href="'.$export_url.'" class="button" target="_blank">'.__('Download Export','linkpro').'</a>';
-	}
+	
 	
 	function admin_init() {
 		
@@ -49,81 +36,7 @@ class linkpro_admin {
 		
 	}
 	
-	function get_pending_verify_requests_count(){
-		$count = 0;
-		
-		// verification status
-		$pending = get_option('linkpro_verify_requests');
-		if (is_array($pending) && count($pending) > 0){
-			$count = count($pending);
-		}
-		
-		// waiting email approve
-		$users = get_users(array(
-			'meta_key'     => '_account_status',
-			'meta_value'   => 'pending',
-			'meta_compare' => '=',
-		));
-		if (isset($users)) {
-			$count += count($users);
-		}
-		
-		// waiting admin approve
-		$users = get_users(array(
-			'meta_key'     => '_account_status',
-			'meta_value'   => 'pending_admin',
-			'meta_compare' => '=',
-		));
-		if (isset($users)) {
-			$count += count($users);
-		}
-		
-		if ($count > 0){
-			return '<span class="upadmin-bubble-new">'.$count.'</span>';
-		}
-	}
 	
-	function get_pending_verify_requests_count_only(){
-		$count = 0;
-		
-		// verification status
-		$pending = get_option('linkpro_verify_requests');
-		if (is_array($pending) && count($pending) > 0){
-			$count = count($pending);
-		}
-		
-		// waiting email approve
-		$users = get_users(array(
-			'meta_key'     => '_account_status',
-			'meta_value'   => 'pending',
-			'meta_compare' => '=',
-		));
-		if (isset($users)) {
-			$count += count($users);
-		}
-		
-		// waiting admin approve
-		$users = get_users(array(
-			'meta_key'     => '_account_status',
-			'meta_value'   => 'pending_admin',
-			'meta_compare' => '=',
-		));
-		if (isset($users)) {
-			$count += count($users);
-		}
-		
-		if ($count > 0){
-			return $count;
-		}
-	}
-	
-	function delete_pending_request($user_id){
-		$arr = get_option('linkpro_verify_requests');
-		if (isset($arr) && is_array($arr)){
-			$arr = array_diff($arr, array( $user_id ));
-			update_option('linkpro_verify_requests', $arr);
-		}
-	}
 	
 	function admin_head(){
 		$screen = get_current_screen();
@@ -178,7 +91,7 @@ class linkpro_admin {
 	
 	function add_menu() {
 	
-		$pending_count = $this->get_pending_verify_requests_count_only();
+		$pending_count = 0;
 		$pending_title = esc_attr( sprintf(__( '%d new verification requests','linkpro'), $pending_count ) );
 		if ($pending_count > 0){
 		$menu_label = sprintf( __( 'Link Pro %s','linkpro' ), "<span class='update-plugins count-$pending_count' title='$pending_title'><span class='update-count'>" . number_format_i18n($pending_count) . "</span></span>" );
@@ -223,61 +136,11 @@ class linkpro_admin {
 		}
 	}
 	
-	function do_action(){
-		global $linkpro;
-		if ($_GET['linkpro_act'] == 'clear_unused_uploads'){
-			$files = glob( $linkpro->upload_base_dir . '*');
-			$i = 0;
-			foreach($files as $file){
-				if(is_file($file)) {
-					$i++;
-					unlink($file);
-				}
-			}
-			echo '<div class="updated"><p><strong>'.sprintf(__('%s files deleted.','linkpro'), $i).'</strong></p></div>';
-		}
-		if ($_GET['linkpro_act'] == 'clear_deleted_users') {
-			$files = glob( $linkpro->upload_base_dir . '*');
-			$i = 0;
-			foreach($files as $file){
-				if(!is_file($file)) {
-					if (!$linkpro->user_exists( basename($file) )) {
-						$i++;
-						$linkpro->delete_folder($file);
-					}
-				}
-			}
-			echo '<div class="updated"><p><strong>'.sprintf(__('%s unused folders deleted.','linkpro'), $i).'</strong></p></div>';
-		}
-		if ($_GET['linkpro_act'] == 'clear_cache') {
-			global $linkpro;
-			$linkpro->clear_cache();
-			echo '<div class="updated"><p><strong>'.sprintf(__('%s Members Cache Clear .','linkpro'), $i).'</strong></p></div>';
-		}
-		if ($_GET['linkpro_act'] == 'reset_online_users') {
-			delete_transient('linkpro_users_online');
-			echo '<div class="updated"><p><strong>'.__('Online users data is reset.','linkpro').'</strong></p></div>';
-		}
-		if ($_GET['linkpro_act'] == 'clear_activity') {
-			delete_option('linkpro_activity');
-			echo '<div class="updated"><p><strong>'.__('Activity stream has been reset.','linkpro').'</strong></p></div>';
-		}
-	}
+	
 	
 	function save() {
 	
-		/* restrict tab */
-		if (isset($_GET['tab']) && $_GET['tab'] == 'restrict'){
-			$this->options['linkpro_restricted_pages'] = '';
-		}
 		
-		/* field roles tab */
-		if (isset($_GET['tab']) && $_GET['tab'] == 'fieldroles'){
-			$fields = get_option('linkpro_fields');
-			foreach($fields as $key => $field){
-				$this->options[$key.'_roles'] = '';
-			}
-		}
 		
 		/* roles that can view profiles */
 		if (isset($_GET['tab']) && $_GET['tab'] == 'settings'){
@@ -321,36 +184,7 @@ class linkpro_admin {
 	}
 		
 	
-	function reinstall(){
-		global $linkpro;
-		
-		// trash current pages
-		$pages = get_option('linkpro_pages') + get_option('linkpro_sc_pages');
-		foreach( $pages as $page_id ) {
-			wp_delete_post( $page_id, true );
-		}
-		
-		// delete existing pages from settings
-		delete_option('linkpro_pages');
-		
-		// trash linkpro options
-		foreach( wp_load_alloptions() as $k => $v) {
-			if (strstr($k, 'linkpro')){
-				delete_option( $k );
-			}
-		}
-		
-		// install default fields again
-		linkpro_init_setup();
 
-		// update icons
-		if (!get_option('linkpro_pre_icons_setup') ) {
-			$linkpro->update_field_icons();
-		}
-		
-		echo '<div class="updated"><p><strong>'.__('linkpro has been reset to factory settings.','linkpro').'</strong></p></div>';
-	}
-	
 
 	
 	function admin_page() {
--- admin/scripts/admin.js
+++ admin/scripts/admin.js
@@ -1,31 +1,6 @@
 jQuery(document).ready(function() {
 	
-	/* Toggle selection of roles in post screen */
-	jQuery(document).on('click', 'input[name=linkpro_edit_restrict]', function(){
-		if (jQuery(this).val() == 'roles'){
-			jQuery('p.restrict_roles').show( function(){
-				jQuery("p.restrict_roles select").removeClass("chzn-done").css('display', 'inline').data('chosen', null);
-				jQuery("p.restrict_roles *[class*=chzn], p.restrict_roles .chosen-container").remove();
-				jQuery("p.restrict_roles .chosen-select").chosen({
-					disable_search_threshold: 10
-				});
-			});
-		} else {
-			jQuery('p.restrict_roles').hide();
-		}
-	});
-
-	if (jQuery('input[name=linkpro_edit_restrict]:checked').val() == 'roles' ){
-			jQuery('p.restrict_roles').show( function(){
-				jQuery("p.restrict_roles select").removeClass("chzn-done").css('display', 'inline').data('chosen', null);
-				jQuery("p.restrict_roles *[class*=chzn], p.restrict_roles .chosen-container").remove();
-				jQuery("p.restrict_roles .chosen-select").chosen({
-					disable_search_threshold: 10
-				});
-			});
-	} else {
 	
-	}
 
 	/* Toggle admin screen headings */
 	if ( jQuery('div.linkpro-admin h3').length <= 3) {
@@ -59,201 +34,8 @@ jQuery(document).ready(function() {
 		jQuery(this).next('table.form-table, .upadmin-panel').hide();
 	});
 	
-	/* deny user registration */
-	jQuery(document).on('click', '.upadmin-user-deny', function(e){
-		e.preventDefault();
-		var link = jQuery(this);
-		var parent = jQuery(this).parents('.upadmin-pending-verify');
-		jQuery.ajax({
-			url: ajaxurl,
-			data: 'action=linkpro_admin_user_deny&user_id=' + jQuery(this).data('user'),
-			dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				parent.fadeOut();
-				if (data.count === '0' || data.count == '' || !data.count) {
-					jQuery('.upadmin-bubble-new').remove();
-				} else {
-					jQuery('.upadmin-bubble-new').html( data.count );
-				}
-				jQuery('.toplevel_page_linkpro').find('span.update-count').html( data.count );
-			}
-		});
-		return false;
-	});
 	
-	/* approve user registration */
-	jQuery(document).on('click', '.upadmin-user-approve', function(e){
-		e.preventDefault();
-		var link = jQuery(this);
-		var parent = jQuery(this).parents('.upadmin-pending-verify');
-		jQuery.ajax({
-			url: ajaxurl,
-			data: 'action=linkpro_admin_user_approve&user_id=' + jQuery(this).data('user'),
-			dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				parent.fadeOut();
-				if (data.count === '0' || data.count == '' || !data.count) {
-					jQuery('.upadmin-bubble-new').remove();
-				} else {
-					jQuery('.upadmin-bubble-new').html( data.count );
-				}
-				jQuery('.toplevel_page_linkpro').find('span.update-count').html( data.count );
-			}
-		});
-		return false;
-	});
-	
-	/* Verify user */
-	jQuery(document).on('click', '.upadmin-verify-u', function(e){
-		e.preventDefault();
-		var link = jQuery(this);
-		var parent = jQuery(this).parents('.upadmin-verify-v2');
-		jQuery.ajax({
-			url: ajaxurl,
-			data: 'action=linkpro_verify_user&user_id=' + link.data('user'),
-			dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				parent.hide().html( data.admin_tpl ).fadeIn();
-			}
-		});
-		return false;
-	});
-	
-	/* Unverify user */
-	jQuery(document).on('click', '.upadmin-unverify-u', function(e){
-		e.preventDefault();
-		var link = jQuery(this);
-		var parent = jQuery(this).parents('.upadmin-verify-v2');
-		jQuery.ajax({
-			url: ajaxurl,
-			data: 'action=linkpro_unverify_user&user_id=' + link.data('user'),
-			dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				parent.hide().html( data.admin_tpl ).fadeIn();
-			}
-		});
-		return false;
-	});
-	
-	/* Verification invite */
-	jQuery(document).on('click', '.upadmin-invite-u', function(e){
-		e.preventDefault();
-		var link = jQuery(this);
-		var parent = jQuery(this).parents('.upadmin-verify-v2');
-		jQuery.ajax({
-			url: ajaxurl,
-			data: 'action=linkpro_verify_invite&user_id=' + link.data('user'),
-			dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				parent.hide().html( data.admin_tpl ).fadeIn();
-			}
-		});
-		return false;
-	});
-	
-	/* Verify user */
-	jQuery(document).on('click', '.upadmin-verify', function(e){
-		e.preventDefault();
-		var link = jQuery(this);
-		var parent = jQuery(this).parents('.upadmin-pending-verify');
-		jQuery.ajax({
-			url: ajaxurl,
-			data: 'action=linkpro_verify_user&user_id=' + jQuery(this).data('user'),
-			dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				parent.fadeOut();
-				if (data.count === '0' || data.count == '' || !data.count) {
-					jQuery('.upadmin-bubble-new').remove();
-				} else {
-					jQuery('.upadmin-bubble-new').html( data.count );
-				}
-				jQuery('.toplevel_page_linkpro').find('span.update-count').html( data.count );
-			}
-		});
-		return false;
-	});
-	
-	/* Unverify user */
-	jQuery(document).on('click', '.upadmin-unverify', function(e){
-		e.preventDefault();
-		var link = jQuery(this);
-		var parent = jQuery(this).parents('.upadmin-pending-verify');
-		jQuery.ajax({
-			url: ajaxurl,
-			data: 'action=linkpro_unverify_user&user_id=' + jQuery(this).data('user'),
-			dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				parent.fadeOut();
-				if (data.count === '0' || data.count == '' || !data.count) {
-					jQuery('.upadmin-bubble-new').remove();
-				} else {
-					jQuery('.upadmin-bubble-new').html( data.count );
-				}
-				jQuery('.toplevel_page_linkpro').find('span.update-count').html( data.count );
-			}
-		});
-		return false;
-	});
-	
-	/*  Block user */
-	jQuery(document).on('click', '.upadmin-block-u', function(e){
-		e.preventDefault();
-		$res = window.confirm( "Are you sure you want to block this user ?" );
-		if(!$res){
-			return;
-		}
-		var link = jQuery(this);
-		var parent = jQuery(this).parents('.upadmin-block-v2');
-		jQuery.ajax({
-			url: ajaxurl,
-			data: 'action=linkpro_block_account&user_id=' + link.data('user'),
-			dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				parent.hide().html( data.admin_tpl ).fadeIn();
-			}
-		});
-		return false;
-	});
-	
-	/* Unblock user */
-	jQuery(document).on('click', '.upadmin-unblock-u', function(e){
-		e.preventDefault();
-		var link = jQuery(this);
-		var parent = jQuery(this).parents('.upadmin-block-v2');
-		jQuery.ajax({
-			url: ajaxurl,
-			data: 'action=linkpro_unblock_account&user_id=' + link.data('user'),
-			dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				parent.hide().html( data.admin_tpl ).fadeIn();
-			}
-		});
-		return false;
-	});
-	/* Mouseenter/leave verify user */
-	jQuery(document).on('mouseenter', '.upadmin-unverify,.upadmin-verify', function(e){
-		jQuery(this).find('span').show();
-	})
-	
-	jQuery(document).on('mouseleave', '.upadmin-unverify,.upadmin-verify', function(e){
-		jQuery(this).find('span').hide();
-	});
 
-	/* cancel field editing */
-	jQuery(document).on('click', '.upadmin-field-zone-cancel', function(e){
-		e.preventDefault();
-		jQuery(this).parents('.upadmin-field-zone').hide();
-		return false;
-	});
 	
 	/* chosen select */
 	jQuery(".chosen-select").chosen({
@@ -296,134 +78,14 @@ jQuery(document).ready(function() {
 		}
 	});
 	
-	/* the main field list actions */
-	jQuery(document).on('click', '#upadmin-sortable-fields .upadmin-field-actions a', function(e){
-		e.preventDefault();
-		var act = jQuery(this).attr('class');
-		var field = jQuery(this).parents('li').attr('id').replace('upadmin-','');
-		var load = jQuery(this).parents('.upadmin-fieldlist').find('.upadmin-loader');
-		
-		if (act == 'upadmin-field-action-remove') {
-			if (!confirm('Are you sure you want to delete field from your fields list?')) return false;
-			load.addClass('loading');
-			jQuery(this).parents('li').fadeOut();
-			jQuery.ajax({
-				url: ajaxurl,
-				data: 'action=linkpro_delete_field&field=' + field,
-				dataType: 'JSON',
-				type: 'POST',
-				success:function(data){
-					load.removeClass('loading');
-					jQuery('span.upadmin-ajax-fieldcount').html( data.count );
-				}
-			});
-		}
-		
-		if (act == 'upadmin-field-action-edit') {
-			jQuery(this).parents('li').find('.upadmin-field-zone').toggle();
-		}
-		
-		return false;
-	});
-	
-	/* blur field edit */
-	jQuery(document).on('change', '#upadmin-sortable-fields .upadmin-field-zone input, #upadmin-sortable-fields .upadmin-field-zone select, #upadmin-sortable-fields .upadmin-field-zone textarea', function(e){
-	
-		var load = jQuery(this).parents('.upadmin-fieldlist').find('.upadmin-loader');
-		load.addClass('loading');
-		var field = jQuery(this).parents('li').attr('id').replace('upadmin-','');		
-		var str = '';
-		jQuery(this).parents('li').find('input[type=text]').each(function(){
-			str = str + '&' + jQuery(this).attr('id').replace(field + '-','') + '=' + jQuery(this).val();
-		});
-		jQuery(this).parents('li').find('textarea').each(function(){
-			str = str + '&' + jQuery(this).attr('id').replace(field + '-','') + '=' + jQuery(this).val();
-		});
 
-		jQuery.ajax({
-			url: ajaxurl,
-			data: 'action=linkpro_update_field&field=' + field + str,
-			//dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				load.removeClass('loading');
-			}
-		});
-		
-	});
+	
 
-	/* click on action of field */
-	jQuery(document).on('click', '.upadmin-groups .upadmin-field-actions a', function(e){
 	
-		e.preventDefault();
-		var form = jQuery(this).parents('.upadmin-tpl').find('form');
-		var act = jQuery(this).attr('class');
-		var proc = jQuery(this).data('proc');
-		if (act == 'upadmin-field-action-remove') {
-			if (!confirm('Are you sure you want to delete field from this group?')) return false;
-			jQuery(this).parents('li').fadeOut(function(){jQuery(this).remove(); form.trigger('submit');});
-		}
-		if (act == 'upadmin-field-action-edit') {
-			jQuery(this).parents('li').find('.upadmin-field-zone').toggle();
-		}
-		if (act == 'upadmin-field-action upadmin-field-action-hideable off') {
-			jQuery(this).removeClass('off').addClass('on');jQuery(this).parents('li').find('input[name=' + jQuery(this).data('key') + '-' + jQuery(this).data('role') + ']').val(1);form.trigger('submit');}
-		if (act == 'upadmin-field-action upadmin-field-action-hideable on') {
-			jQuery(this).removeClass('on').addClass('off');jQuery(this).parents('li').find('input[name=' + jQuery(this).data('key') + '-' + jQuery(this).data('role') + ']').val(0);form.trigger('submit');}
-		if (act == 'upadmin-field-action upadmin-field-action-hidden off') {
-			jQuery(this).removeClass('off').addClass('on');jQuery(this).parents('li').find('input[name=' + jQuery(this).data('key') + '-' + jQuery(this).data('role') + ']').val(1);form.trigger('submit');}
-		if (act == 'upadmin-field-action upadmin-field-action-hidden on') {
-			jQuery(this).removeClass('on').addClass('off');jQuery(this).parents('li').find('input[name=' + jQuery(this).data('key') + '-' + jQuery(this).data('role') + ']').val(0);form.trigger('submit');}
-		if (act == 'upadmin-field-action upadmin-field-action-required off') {
-			jQuery(this).removeClass('off').addClass('on');jQuery(this).parents('li').find('input[name=' + jQuery(this).data('key') + '-' + jQuery(this).data('role') + ']').val(1);form.trigger('submit');}
-		if (act == 'upadmin-field-action upadmin-field-action-required on') {
-			jQuery(this).removeClass('on').addClass('off');jQuery(this).parents('li').find('input[name=' + jQuery(this).data('key') + '-' + jQuery(this).data('role') + ']').val(0);form.trigger('submit');}
-		if (act == 'upadmin-field-action upadmin-field-action-locked off') {
-			jQuery(this).removeClass('off').addClass('on');jQuery(this).parents('li').find('input[name=' + jQuery(this).data('key') + '-' + jQuery(this).data('role') + ']').val(1);form.trigger('submit');}
-		if (act == 'upadmin-field-action upadmin-field-action-locked on') {
-			jQuery(this).removeClass('on').addClass('off');jQuery(this).parents('li').find('input[name=' + jQuery(this).data('key') + '-' + jQuery(this).data('role') + ']').val(0);form.trigger('submit');}
-		if (act == 'upadmin-field-action upadmin-field-action-private off') {
-			jQuery(this).removeClass('off').addClass('on');jQuery(this).parents('li').find('input[name=' + jQuery(this).data('key') + '-' + jQuery(this).data('role') + ']').val(1);form.trigger('submit');}
-		if (act == 'upadmin-field-action upadmin-field-action-private on') {
-			jQuery(this).removeClass('on').addClass('off');jQuery(this).parents('li').find('input[name=' + jQuery(this).data('key') + '-' + jQuery(this).data('role') + ']').val(0);form.trigger('submit');}
-		if (act == 'upadmin-field-action upadmin-field-action-html off') {
-			jQuery(this).removeClass('off').addClass('on');jQuery(this).parents('li').find('input[name=' + jQuery(this).data('key') + '-' + jQuery(this).data('role') + ']').val(1);form.trigger('submit');}
-		if (act == 'upadmin-field-action upadmin-field-action-html on') {
-			jQuery(this).removeClass('on').addClass('off');jQuery(this).parents('li').find('input[name=' + jQuery(this).data('key') + '-' + jQuery(this).data('role') + ']').val(0);form.trigger('submit');}
-		return false;
-	});
-	
-	/* blur field edit */
-	jQuery(document).on('change', '.upadmin-groups .upadmin-field-zone input, .upadmin-groups .upadmin-field-zone select, .upadmin-groups .upadmin-field-zone textarea', function(e){
-		var form = jQuery(this).parents('.upadmin-tpl').find('form');
-		form.trigger('submit');
-	});
 
-	/* toggle adding new field */
-	jQuery(document).on('click', '.upadmin-toggle-new', function(e){
-		e.preventDefault();
-		var new_field = jQuery('.upadmin-new');
-		if (new_field.is(':hidden')){
-		new_field.show();
-		
-			/* chosen dropdowns */
-			jQuery(".upadmin-new select").removeClass("chzn-done").css('display', 'inline').data('chosen', null);
-			jQuery(".upadmin-new *[class*=chzn], .upadmin-new .chosen-container").remove();
-			jQuery(".upadmin-new .chosen-select").chosen({
-				disable_search_threshold: 10
-			});
-			
-		}else{
-		new_field.hide();
-		}
-		return false;
-	});
+
 	
-	/* icon clicks */
-	jQuery(document).on('click', '.upadmin-icon-abs a:not(.upadmin-noajax)', function(e){
-		e.preventDefault();
-		return false;
-	});
+
 
 	/* toggle/un-toggle field groups */
 	jQuery(document).on('click', '.upadmin-icon-abs a.max', function(e){
@@ -448,50 +110,9 @@ jQuery(document).ready(function() {
 		return false;
 	});
 	
-	/* reset original fields */
-	jQuery(document).on('click', '.upadmin-reset-fields', function(e){
 	
-		e.preventDefault();
-		form = jQuery(this).parents('.upadmin-fieldlist');
-		if (!confirm('This will restore original plugin fields. Are you sure?')) return false;
-		form.find('.upadmin-loader').addClass('loading');
-		jQuery.ajax({
-			url: ajaxurl,
-			data: 'action=linkpro_restore_builtin_fields',
-			dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				form.find('.upadmin-loader').removeClass('loading');
-				jQuery('span.upadmin-ajax-fieldcount').html( data.count );
-				jQuery('ul#upadmin-sortable-fields').html( data.html );
-			}
-		});
-		return true;
-	});
 
-	/* reset all groups */
-	jQuery(document).on('click', '.upadmin-reset-groups', function(e){
-		e.preventDefault();
-		form = jQuery(this).parents('.upadmin-groups');
-		if (!confirm('This will restore original fields for ALL groups. Are you sure?')) return false;
-		form.find('.upadmin-loader').addClass('loading');
-		jQuery.ajax({
-			url: ajaxurl,
-			data: 'action=linkpro_restore_builtin_groups',
-			dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				form.find('.upadmin-loader').removeClass('loading');
-				jQuery('.upadmin-groups-view').html( data.html );
-				jQuery('.upadmin-tpl-body ul').sortable({
-					receive: function(e,ui) {
-						copyHelper= null;
-					}
-				});
-			}
-		});
-		return true;
-	});
+	
 	
 	/* Publish new field */
 	jQuery(document).on('submit', '.upadmin-new form', function(e){
@@ -521,30 +142,7 @@ jQuery(document).ready(function() {
 		return false;
 	});
 	
-	/* reset single group */
-	jQuery(document).on('click', '.upadmin-tpl a.resetgroup', function(e){
-		e.preventDefault();
-		if (!confirm('This will restore original fields for this GROUP. Are you sure?')) return false;
-		var form = jQuery(this).parents('.upadmin-tpl').find('form');
-		var role = form.data('role');
-		form.find('.upadmin-tpl-head').append('<img src="'+form.data('loading')+'" alt="" class="upadmin-miniload" />');
-		jQuery.ajax({
-			url: ajaxurl,
-			data: form.serialize() + '&action=linkpro_reset_group&role='+role,
-			dataType: 'JSON',
-			type: 'POST',
-			success:function(data){
-				form.find('.upadmin-miniload').remove();
-				form.parents('.upadmin-tpl').replaceWith( data.html );
-				form.parents('.upadmin-tpl').find('.upadmin-tpl-body ul').sortable({
-					receive: function(e,ui) {
-						copyHelper= null;
-					}
-				});
-			}
-		});
-		return false;
-	});
+
 	
 	/* Save forms */
 	jQuery(document).on('click', '.upadmin-tpl a.saveform', function(e){
@@ -572,67 +170,8 @@ jQuery(document).ready(function() {
 		return false;
 	});
 
-	/* The groups that will receive fields */
-	jQuery('.upadmin-tpl-body ul').sortable({
-        receive: function(e,ui) {
-            copyHelper= null;
-			var list = jQuery(this).parents('.upadmin-tpl-body');
-			jQuery.each( list.find("li[data-special^='newsection']"), function(i, v){
-				section_word = 'newsection' + i;
-				jQuery(this).data('special', section_word);
-				jQuery(this).find('input').each(function(){
-					jQuery(this).attr('name', jQuery(this).attr('name').replace('newsection', section_word));
-					jQuery(this).attr('id', jQuery(this).attr('id').replace('newsection', section_word));
-				});
-			});
-        }
-	});
+
 	
-	/* Add new section field */
-	jQuery('ul#upadmin-newsection').sortable({
-		connectWith: ".upadmin-tpl-body ul",
-		forcePlaceholderSize: false,
-		helper: function(e,li) {
-			copyHelper= li.clone().insertAfter(li);
-			return li.clone();
-		},
-		stop: function() {
-			copyHelper && copyHelper.remove();
-		}
-	});
 	
-	/* Moving out field/sorting between fields */
-	var itemList = jQuery('ul#upadmin-sortable-fields');
-	itemList.sortable({
-		connectWith: ".upadmin-tpl-body ul",
-		forcePlaceholderSize: false,
-		helper: function(e,li) {
-			copyHelper= li.clone().insertAfter(li);
-			return li.clone();
-		},
-		stop: function() {
-			copyHelper && copyHelper.remove();
-		},
-        update: function(event, ui) {
-            opts = {
-                url: ajaxurl,
-                type: 'POST',
-                async: true,
-                cache: false,
-                dataType: 'json',
-                data:{
-                    action: 'linkpro_field_sort',
-                    order: itemList.sortable('toArray').toString()
-                },
-                success: function(data) {
-                    return; 
-                },
-                error: function(xhr,textStatus,e) {
-                    return; 
-                }
-            };
-            jQuery.ajax(opts);
-        }
-	});
 	
 });
